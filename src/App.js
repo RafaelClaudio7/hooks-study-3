936c41c14c68b688c941f81a5f3a60201fce5efe
Rafael Tula <rc7true@gmail.com>
2022-07-06 20:55:14 -0300

    useReducer intitial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
+
+const globalState = {
+  title: "O titulo do contexto",
+  body: "O corpo do contexto",
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "muda": {
+      console.log("Chamou muda");
+      return { ...state, title: "Mudou" };
+    }
+  }
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button onClick={() => dispatch({ type: "muda" })}>Click</button>
     </div>
   );
 }
