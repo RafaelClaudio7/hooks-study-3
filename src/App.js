0a673a94e9c60dcb6fa7ac11cf9bf8f91f4b7237
Rafael Tula <rc7true@gmail.com>
2022-07-07 11:03:18 -0300

    Review useReduce by React Docs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import { useReducer } from "react";
 import "./App.css";
 
-const globalState = {
-  title: "O titulo do contexto",
-  body: "O corpo do contexto",
-  counter: 0,
-};
+const initialState = { count: 0 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "muda": {
-      console.log("Chamou muda");
-      return { ...state, title: "Mudou" };
-    }
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
   }
-  return { ...state };
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, globalState);
-  const { counter, title, body } = state;
-
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count} = state;
   return (
     <div className="App">
-      <h1>
-        {title} {counter}
-      </h1>
-      <button onClick={() => dispatch({ type: "muda" })}>Click</button>
+      <h1>Contador: {count}</h1>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
     </div>
   );
 }
