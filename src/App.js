299bf9e77d3b356ff60ab59aa3198d1b420436ff
Rafael Tula <rc7true@gmail.com>
2022-07-13 13:48:41 -0300

    useContext with useReducer example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import { useReducer } from "react";
-import "./App.css";
+import P from 'prop-types';
+import { createContext, useContext, useReducer, useRef } from 'react';
+import './App.css';
 
-const initialState = { count: 0 };
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    default:
-      throw new Error();
-  }
+// actions.js
+export const actions = {
+  CHANGE_TITLE: 'CHANGE_TITLE',
 };
 
-function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { count} = state;
+// data.js
+export const globalState = {
+  title: 'O título que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+// reducer.js
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.CHANGE_TITLE: {
+      console.log('Mudar título');
+      return { ...state, title: action.payload };
+    }
+  }
+
+  return { ...state };
+};
+
+// AppContext.jsx
+export const Context = createContext();
+export const AppContext = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, globalState);
+
+  const changeTitle = (payload) => {
+    dispatch({ type: actions.CHANGE_TITLE, payload });
+  };
+
   return (
-    <div className="App">
-      <h1>Contador: {count}</h1>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-    </div>
+    <Context.Provider value={{ state, changeTitle }}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+AppContext.propTypes = {
+  children: P.node,
+};
+
+// H1/index.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+  const inputRef = useRef();
+
+  return (
+    <>
+      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>
+        {context.state.title}
+      </h1>
+      <input type="text" ref={inputRef} />
+    </>
+  );
+};
+
+// App.jsx
+function App() {
+  return (
+    <AppContext>
+      <div>
+        <H1 />
+      </div>
+    </AppContext>
   );
 }
 
